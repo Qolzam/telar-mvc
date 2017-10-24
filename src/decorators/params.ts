bcf10d3bb5b32e19653c0ee3f3d6ef5e2731df0d
Sylvain Estevez <sylvain@luxe.com>
2017-10-24 09:31:33 -0700

    :sparkles: Shorcut schema for @params

--- src/decorators/params.ts
+++ src/decorators/params.ts
@@ -5,13 +5,14 @@ import { MetadataKey } from '../constants/metadata-key';
 import { translateAjvError } from '../utils/translate-ajv-error';
 import { TParamsOptions } from '../types/params-options';
 import { TJSONSchema } from '@bluejay/schema';
+import { isJSONSchema } from '../utils/is-json-schema';
 
 const defaultAjvInstance = new AJV({ coerceTypes: true });
 const defaultAjvFactory = () => defaultAjvInstance;
 
-export function params(options: TParamsOptions) {
-  const { ajvFactory, jsonSchema } = options;
-  const ajvInstance = (ajvFactory || defaultAjvFactory)();
+export function params(options: TParamsOptions | TJSONSchema) {
+  const jsonSchema = isJSONSchema(options) ? options : (<TParamsOptions>options).jsonSchema;
+  const ajvInstance = ((<TParamsOptions>options).ajvFactory || defaultAjvFactory)();
   const validator = ajvInstance.compile(jsonSchema);
 
   return function(target: any, key: string, descriptor: PropertyDescriptor) {
--- test/decorators/params.test.ts
+++ test/decorators/params.test.ts
@@ -11,35 +11,64 @@ import { Sandbox } from '../resources/classes/sandbox';
 import { errorHandler } from '../resources/middlewares/error-handler';
 
 describe('@params()', () => {
-  const id = Symbol();
+  function doTest(sandbox: Sandbox) {
+    it('should coerce integer', async () => {
+      await supertest(sandbox.getApp())
+        .get('/test/12')
+        .expect(StatusCode.OK, { id: 12 });
+    });
 
-  @path('/test')
-  @after(errorHandler)
-  class TestController extends Controller {
-    @get('/:id')
-    @params({
-      jsonSchema: requireProperties(object({ id: integer() }), ['id'])
-    })
-    private async getById(req: Request, res: Response) {
-      res.status(StatusCode.OK).json(req.params);
-    }
+    it('should not coerce string', async () => {
+      await supertest(sandbox.getApp())
+        .get('/test/true')
+        .expect(StatusCode.BAD_REQUEST);
+    });
   }
 
-  const sandbox = new Sandbox({
-    controllersMap: new Map([
-      [id, TestController]
-    ])
+  describe('Via options', () => {
+    const id = Symbol();
+
+    @path('/test')
+    @after(errorHandler)
+    class TestController extends Controller {
+      @get('/:id')
+      @params({
+        jsonSchema: requireProperties(object({ id: integer() }), ['id'])
+      })
+      private async getById(req: Request, res: Response) {
+        res.status(StatusCode.OK).json(req.params);
+      }
+    }
+
+    const sandbox = new Sandbox({
+      controllersMap: new Map([
+        [id, TestController]
+      ])
+    });
+
+    doTest(sandbox);
   });
 
-  it('should coerce integer', async () => {
-    await supertest(sandbox.getApp())
-      .get('/test/12')
-      .expect(StatusCode.OK, { id: 12 });
+  describe('Schema only', () => {
+    const id = Symbol();
+
+    @path('/test')
+    @after(errorHandler)
+    class TestController extends Controller {
+      @get('/:id')
+      @params(requireProperties(object({ id: integer() }), ['id']))
+      private async getById(req: Request, res: Response) {
+        res.status(StatusCode.OK).json(req.params);
+      }
+    }
+
+    const sandbox = new Sandbox({
+      controllersMap: new Map([
+        [id, TestController]
+      ])
+    });
+
+    doTest(sandbox);
   });
 
-  it('should not coerce string', async () => {
-    await supertest(sandbox.getApp())
-      .get('/test/true')
-      .expect(StatusCode.BAD_REQUEST);
-  });
 });
