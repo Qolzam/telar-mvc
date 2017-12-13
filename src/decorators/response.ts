36c30a3f3e9373b4daa3b8dbaa54552cf214ef3f
Sylvain Estevez <sylvain@luxe.com>
2017-12-12 18:25:02 -0800

    :card_index: Add metdata for non json res

--- src/decorators/response.ts
+++ src/decorators/response.ts
@@ -4,13 +4,20 @@ import { jsonResponse } from './json-response';
 import { TJSONResponseOptions } from '../types/json-response-options';
 import { NextFunction, Request, Response } from 'express';
 import { before } from './before';
+import { MetadataKey } from '../constants/metadata-key';
 
 export function response(options: TCustomResponseOptions | TJSONResponseOptions) {
   if (isJSONResponseOptions(options)) {
     return jsonResponse(options);
   }
 
+  const isStatusCodesArray = Array.isArray(options.statusCode);
+
   return function(target: any, key: string, descriptor: PropertyDescriptor) {
+    Reflect.defineMetadata(MetadataKey.ROUTE_RESPONSE, {
+      statusCodes: isStatusCodesArray ? options.statusCode : [options.statusCode],
+      contentType: options.contentType
+    }, target, key);
     before((req: Request, res: Response, next: NextFunction) => {
       res.status(options.statusCode as number);
       res.set('Content-Type', options.contentType);
