2c4ca0f8dac248246d0aa9b96c10e327d9fa060e
Nicolas Gramlich <nicolas.gramlich@volvocars.com>
2018-01-23 15:26:59 -0800

    :fire: Removed isJSONSchema helper.

--- src/utils/is-json-schema.ts
+++
@@ -1,6 +0,0 @@
-import { TJSONSchema } from '@bluejay/schema';
-import * as Lodash from 'lodash';
-
-export function isJSONSchema(obj: {}): obj is TJSONSchema {
-  return Lodash.isPlainObject(obj) && `type` in obj;
-}
