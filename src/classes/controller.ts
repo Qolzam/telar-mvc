a41136410bcb90c53469c4a460e14c3a94630904
Sylvain Estevez <sylvain@luxe.com>
2018-03-08 14:51:26 +0100

    :scissors: Controllers dont need their own router anymore

--- src/classes/controller.ts
+++ src/classes/controller.ts
@@ -1,19 +1,9 @@
 import { IController } from '../interfaces/controller';
-import { Router } from 'express';
 import { injectable } from 'inversify';
 
 @injectable()
 export class Controller implements IController {
-  protected router: Router;
   protected path: string = '/';
-
-  public constructor() {
-    this.router = Router();
-  }
-
-  public getRouter() {
-    return this.router;
-  }
 
   public getPath() {
     return this.path;
--- src/interfaces/controller.ts
+++ src/interfaces/controller.ts
@@ -1,6 +1,3 @@
-import { Router } from 'express';
-
 export interface IController {
   getPath(): string;
-  getRouter(): Router;
 }
