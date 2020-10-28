757c5eb87932e0767979b09485ddee96fa40e06b
Sylvain Estevez <sylvain@luxe.com>
2020-10-28 09:37:59 +0100

    :shirt: Cleanup

--- src/decorators/get.ts
+++ src/decorators/get.ts
@@ -2,7 +2,7 @@ import { HTTPMethod } from '@bluejay/http-method';
 import { method } from './method';
 
 /* tslint:disable-next-line:no-unused-variable */
-import { IController } from  '../interfaces/controller'; // Keep
+import { IController } from  '../interfaces/controller'; // Keep!!!
 
 export function get(path: string) {
   return method(HTTPMethod.GET, path);
